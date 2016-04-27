fa55e112e0c59757626281dae27f3ed759a7c553
Ram Narayan <patroramnarayan1@gmail.com>
2016-04-27 12:25:40 +0530

    First commit for jenkins-Github integretion

---
+++ hello.java
@@ -0,0 +1,15 @@
+public class hello
+{
+	public void methodA(){
+	System.out.println("My world");
+	}
+	
+	public static void main(String args[])
+	{
+		hello he = new hello();
+		he.methodA();
+		
+	}
+	
+	
+}
